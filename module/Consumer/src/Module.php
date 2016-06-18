c6e08f80c4ce36cff54e9936a7d2b75e4d556b7c
Lorenzo Ferrara Junior <lorenzoferrarajr@gmail.com>
2016-06-18 18:12:17 +0200

    Show console banner and usage

--- module/Consumer/src/Module.php
+++ module/Consumer/src/Module.php
@@ -8,7 +8,7 @@ use Zend\ModuleManager\Feature\ConsoleBannerProviderInterface;
 use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Zend\Console\Adapter\AdapterInterface as Console;
 
-class Module implements ConfigProviderInterface
+class Module implements ConfigProviderInterface, ConsoleUsageProviderInterface, ConsoleBannerProviderInterface
 {
     const VERSION = '0.0.1';
 
@@ -17,20 +17,20 @@ class Module implements ConfigProviderInterface
         return include __DIR__ . '/../config/module.config.php';
     }
 
-//    public function getConsoleUsage(Console $console)
-//    {
-//        return [
-//            // Describe available commands
-//            'user resetpassword [--verbose|-v] EMAIL' => 'Reset password for a user',
-//
-//            // Describe expected parameters
-//            [ 'EMAIL',        'Email of the user for a password reset' ],
-//            [ '--verbose|-v', '(optional) turn on verbose mode'        ],
-//        ];
-//    }
+    public function getConsoleUsage(Console $console)
+    {
+        return [
+            // Describe available commands
+            'user resetpassword [--verbose|-v] EMAIL' => 'Reset password for a user',
 
-//    public function getConsoleBanner(Console $console)
-//    {
-//        return 'Consumer '.self::VERSION;
-//    }
+            // Describe expected parameters
+            [ 'EMAIL',        'Email of the user for a password reset' ],
+            [ '--verbose|-v', '(optional) turn on verbose mode'        ],
+        ];
+    }
+
+    public function getConsoleBanner(Console $console)
+    {
+        return 'Consumer '.self::VERSION;
+    }
 }
